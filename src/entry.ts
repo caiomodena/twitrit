b81f13860688e151f39e5d0169dcf1f3d2f16dff
alexsandro0203 <alexsandro0203@hotmail.com>
2019-07-16 17:24:43 -0300

    Ajustando portas da aplicação

--- src/entry.ts
+++ src/entry.ts
@@ -1,6 +1,6 @@
 import server from './server';
 
 server.set('port', process.env.APP_PORT || 3007)
-server.listen(3000, () => {
-    console.log(`[SERVER] Running at http://localhost:3000`);
+server.listen(3007, () => {
+    console.log(`[SERVER] Running at http://localhost:3007`);
 });
--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -1,7 +1,16 @@
 import req from 'supertest';
 
 import server from "../server";
-test("[GET] /", async () => {
+test("Hello node test", async () => {
     const res = await req(server).get("/hello");
     expect(res.text).toBe("Hello node!");
+});
+
+test("Deverá ser possível incluir usuário.", async () => {
+    const res = await req(server).post("/user")
+        .send({name: 'alexsandro'})
+        .expect('teste')
+
+
+    //expect(res.text).toBe("Hello node!");
 });
