0bcd5db3fac630c525df9aa1f5c62b732c9de4fa
Alexsandro da Silva Ferrão <alexsandro.ferrao@e-unicred.com.br>
2019-07-17 23:04:49 -0300

    Ajustanto teste para ser finalizado

--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -2,14 +2,16 @@ import req from 'supertest';
 import server from "../server";
 
 describe('Comportamento do usuário', () => {
-    test("Hello node test", async () => {
+    test("Hello node test", async (done) => {
         const res = await req(server).get("/hello");
             expect(res.text).toBe("Hello node!");
+        done();
     });
 
-    test("Deverá ser possível incluir usuário.", async () => {
+    test("Deverá ser possível incluir usuário.",  (done) => {
         req(server).post("/user")
             .send({name: 'testeNome', email: 'teste@teste.com.br'})
-            .expect(201)
+            .expect(201);
+        done();
     });
-}
+});
