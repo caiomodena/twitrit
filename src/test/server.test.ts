227961794175f35c76af4dafb92d746724730774
Alexsandro da Silva Ferrão <alexsandro.ferrao@e-unicred.com.br>
2019-07-18 20:44:47 -0300

    Resolvendo conflito merge

--- src/test/server.test.ts
+++
@@ -1,7 +0,0 @@
-import req from 'supertest';
-
-import server from "../server";
-test("Hello node test", async () => {
-    const res = await req(server).get("/hello");
-    expect(res.text).toBe("Hello node!");
-});
