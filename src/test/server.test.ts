d132449723bff4f38e0d6b67c27da894ab5d65d7
Alexsandro da Silva Ferrão <alexsandro.ferrao@e-unicred.com.br>
2019-07-17 22:33:35 -0300

    Ajuste no mongose e ajuste do teste

--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -1,16 +1,14 @@
 import req from 'supertest';
-
 import server from "../server";
-test("Hello node test", async () => {
-    const res = await req(server).get("/hello");
-    expect(res.text).toBe("Hello node!");
-});
 
-test("Deverá ser possível incluir usuário.", async () => {
-    const res = await req(server).post("/user")
-        .send({name: 'alexsandro'})
-        .expect('teste')
+describe('Comportamento do usuário', () => {
+    test("Hello node test", async () => {
+        const res = await req(server).get("/hello");
+            expect(res.text).toBe("Hello node!");
+    });
 
-
-    //expect(res.text).toBe("Hello node!");
-});
+    test("Deverá ser possível incluir usuário.", async () => {
+        await req(server).post("/user",{name: 'testeNome', email: 'teste@teste.com.br'})
+            .expect(201)
+    });
+}
